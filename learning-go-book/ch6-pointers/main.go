ebf69443d3c47231110d278c2a8c94f53cf00b63
Paul Tobin <tobster111@gmail.com>
2025-11-03 19:07:15 +0000

    Refactor main.go to comment out unused pointer examples and retain dereferencing demonstration

--- learning-go-book/ch6-pointers/main.go
+++ learning-go-book/ch6-pointers/main.go
@@ -1,23 +1,38 @@
 package main
 
+import "fmt"
+
 func main() {
 	// learn about pointers in Go
-	var x int32 = 10
-	var y bool = true
+	// var x int32 = 10
+	// var y bool = true
 
-	pointerX := &x
-	pointerY := &y
-	var pointerZ *string
+	// pointerX := &x
+	// pointerY := &y
+	// var pointerZ *string
 
-	println("Value of x:", x)
-	println("Address of x:", pointerX)
-	println("Value at address of x:", *pointerX)
+	// println("Value of x:", x)
+	// println("Address of x:", pointerX)
+	// println("Value at address of x:", *pointerX)
 
-	println("Value of y:", y)
-	println("Address of y:", pointerY)
-	println("Value at address of y:", *pointerY)
+	// println("Value of y:", y)
+	// println("Address of y:", pointerY)
+	// println("Value at address of y:", *pointerY)
 
-	println("Value of pointerZ (should be nil):", pointerZ)
+	// println("Value of pointerZ (should be nil):", pointerZ)
 
-	
+	// x := "hello"
+	// pointerX := &x
+	// println("Before change, x:", x)
+	// *pointerX = "world"
+	// println("After change, x:", x)
+
+	// dereferencing
+	x := 10
+	pointerToX := &x
+	fmt.Println(pointerToX)
+	fmt.Println(*pointerToX)
+
+	z := 20 + *pointerToX
+	fmt.Println(z)
 }
