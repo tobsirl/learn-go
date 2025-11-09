5b89f669f27fa3c474e7d1cee44bcfadd6fa86e9
Paul Tobin <tobster111@gmail.com>
2025-11-09 14:23:57 +0000

    Refactor MyFuncOpts struct to include JSON tags and add ToJSON/FromJSON methods

--- learning-go-book/ch5-functions/functions.go
+++ learning-go-book/ch5-functions/functions.go
@@ -1,11 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+)
 
 type MyFuncOpts struct {
-	FirstName string
-	LastName string
-	Age int
+	FirstName string `json:"first_name"`
+	LastName  string `json:"last_name"`
+	Age       int    `json:"age"`
 }
 
 func MyFunc(opts MyFuncOpts) error  {
@@ -14,16 +18,52 @@ func MyFunc(opts MyFuncOpts) error  {
 	return nil
 }
 
+// ToJSON converts the struct to JSON string
+func (m MyFuncOpts) ToJSON() (string, error) {
+	jsonBytes, err := json.Marshal(m)
+	if err != nil {
+		return "", err
+	}
+	return string(jsonBytes), nil
+}
+
+// FromJSON creates a MyFuncOpts from JSON string
+func FromJSON(jsonStr string) (MyFuncOpts, error) {
+	var opts MyFuncOpts
+	err := json.Unmarshal([]byte(jsonStr), &opts)
+	return opts, err
+}
+
 func main() {
-	MyFunc(MyFuncOpts{
+	// Example 1: Regular struct usage
+	person1 := MyFuncOpts{
 		FirstName: "John",
 		LastName: "Doe",
 		Age: 30,
-	})
+	}
+	MyFunc(person1)
 
-	MyFunc(MyFuncOpts{
-		FirstName: "Jane",
-		LastName: "Smith",
-		Age: 25,
-	})
+	// Example 2: Convert struct to JSON
+	jsonStr, err := person1.ToJSON()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("JSON representation:", jsonStr)
+
+	// Example 3: Create struct from JSON string
+	jsonData := `{"first_name":"Jane","last_name":"Smith","age":25}`
+	person2, err := FromJSON(jsonData)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Created from JSON:")
+	MyFunc(person2)
+
+	// Example 4: Pretty print JSON
+	prettyJSON, err := json.MarshalIndent(person1, "", "  ")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Pretty JSON:")
+	fmt.Println(string(prettyJSON))
 }
