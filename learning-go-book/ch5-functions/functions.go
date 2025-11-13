a227f2eb942c6e542d1a6ae3689cff31e69b23da
Paul Tobin <tobster111@gmail.com>
2025-11-13 20:12:49 +0000

    Refactor main.go to comment out example usages of MyFunc and MyFuncOpts

--- learning-go-book/ch5-functions/functions.go
+++ learning-go-book/ch5-functions/functions.go
@@ -3,7 +3,6 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"log"
 )
 
 type MyFuncOpts struct {
@@ -12,8 +11,8 @@ type MyFuncOpts struct {
 	Age       int    `json:"age"`
 }
 
-func MyFunc(opts MyFuncOpts) error  {
-	fmt.Println("Name:", opts.FirstName + " " + opts.LastName)
+func MyFunc(opts MyFuncOpts) error {
+	fmt.Println("Name:", opts.FirstName+" "+opts.LastName)
 	fmt.Println("Age:", opts.Age)
 	return nil
 }
@@ -36,35 +35,41 @@ func FromJSON(jsonStr string) (MyFuncOpts, error) {
 
 func main() {
 	// Example 1: Regular struct usage
-	person1 := MyFuncOpts{
-		FirstName: "John",
-		LastName: "Doe",
-		Age: 30,
-	}
-	MyFunc(person1)
+	// person1 := MyFuncOpts{
+	// 	FirstName: "John",
+	// 	LastName: "Doe",
+	// 	Age: 30,
+	// }
+	// MyFunc(person1)
 
-	// Example 2: Convert struct to JSON
-	jsonStr, err := person1.ToJSON()
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println("JSON representation:", jsonStr)
+	// // Example 2: Convert struct to JSON
+	// jsonStr, err := person1.ToJSON()
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// fmt.Println("JSON representation:", jsonStr)
 
-	// Example 3: Create struct from JSON string
-	jsonData := `{"first_name":"Jane","last_name":"Smith","age":25}`
-	person2, err := FromJSON(jsonData)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println("Created from JSON:")
-	MyFunc(person2)
+	// // Example 3: Create struct from JSON string
+	// jsonData := `{"first_name":"Jane","last_name":"Smith","age":25}`
+	// person2, err := FromJSON(jsonData)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// fmt.Println("Created from JSON:")
+	// MyFunc(person2)
 
-	// Example 4: Pretty print JSON
-	prettyJSON, err := json.MarshalIndent(person1, "", "  ")
-	if err != nil {
-		log.Fatal(err)
+	// // Example 4: Pretty print JSON
+	// prettyJSON, err := json.MarshalIndent(person1, "", "  ")
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// fmt.Println("Pretty JSON:")
+	// fmt.Println(string(prettyJSON))
+
+	f := func(j int) {
+		fmt.Println("printing", j, "from inside of an anonymous function")
 	}
-	fmt.Println("Pretty JSON:")
-	fmt.Println(string(prettyJSON))
-	
+	for i := 0; i < 5; i++ {
+		f(i)
+	}
 }
