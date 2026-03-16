90ee429b81154f41f2cca9589e8d6ed1223616a7
Paul Tobin <tobster111@gmail.com>
2026-03-16 09:40:59 +0000

    Add unit tests for EachCons function to validate consecutive subsets generation

--- codewars/cascading_subsets/cascading_subsets_test.go
+++ codewars/cascading_subsets/cascading_subsets_test.go
@@ -1,1 +1,25 @@
 package cascadingsubsets
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestEachCons(t *testing.T) {
+	tests := []struct {
+		arr      []int
+		n        int
+		expected [][]int
+	}{
+		{[]int{1, 2, 3, 4}, 2, [][]int{{1, 2}, {2, 3}, {3, 4}}},
+		{[]int{1, 2, 3, 4}, 3, [][]int{{1, 2, 3}, {2, 3, 4}}},
+		{[]int{1, 2, 3}, 1, [][]int{{1}, {2}, {3}}},
+	}
+
+	for _, test := range tests {
+		result := EachCons(test.arr, test.n)
+		if !reflect.DeepEqual(result, test.expected) {
+			t.Errorf("EachCons(%v, %d) = %v; expected %v", test.arr, test.n, result, test.expected)
+		}
+	}
+}	
