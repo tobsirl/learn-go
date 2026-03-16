7d8fb9bee5977f4da6caa95ce5ff4856ecaaba49
Paul Tobin <tobster111@gmail.com>
2026-03-16 09:40:07 +0000

    Add EachCons function to generate consecutive subsets of a given length

---
+++ codewars/cascading_subsets/cascading_subsets.go
@@ -0,0 +1,9 @@
+package cascadingsubsets
+
+func EachCons(arr []int, n int) [][]int {
+	var result [][]int
+	for i := 0; i <= len(arr)-n; i++ {
+		result = append(result, arr[i:i+n])
+	}
+	return result
+}
