6f3ba7c86699ae4359f9c54b1fbf9b6c10350f6a
Paul Tobin <tobster111@gmail.com>
2026-03-18 18:30:26 +0000

    Add unit tests for MinMax function to validate minimum and maximum values

--- codewars/the_highest_profit_wins/the_highest_profit_wins_test.go
+++ codewars/the_highest_profit_wins/the_highest_profit_wins_test.go
@@ -1,1 +1,22 @@
 package thehighestprofitwins
+
+import "testing"
+
+func TestMinMax(t *testing.T) {
+	tests := []struct {
+		arr      []int
+		expected [2]int
+	}{
+		{[]int{1, 2, 3, 4, 5}, [2]int{1, 5}},
+		{[]int{-1, -2, -3, -4, -5}, [2]int{-5, -1}},
+		{[]int{5}, [2]int{5, 5}},
+		{[]int{}, [2]int{0, 0}},
+	}
+
+	for _, test := range tests {
+		result := MinMax(test.arr)
+		if result != test.expected {
+			t.Errorf("MinMax(%v) = %v; expected %v", test.arr, result, test.expected)
+		}
+	}
+}
