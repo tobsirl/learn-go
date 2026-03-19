bfc0e203cd1982959560ce2d4c24b5b53f51c3fe
Paul Tobin <tobster111@gmail.com>
2026-03-19 18:10:23 +0000

    Add unit tests for Grow function to validate product calculation of an array

--- codewars/reduce_but_grow/reduce_but_grow_test.go
+++ codewars/reduce_but_grow/reduce_but_grow_test.go
@@ -1,1 +1,22 @@
 package reducebutgrow
+
+import "testing"
+
+func TestGrow(t *testing.T) {
+	tests := []struct {
+		arr      []int
+		expected int
+	}{
+		{[]int{1, 2, 3}, 6},
+		{[]int{4, 5, 6}, 120},
+		{[]int{7, 8, 9}, 504},
+		{[]int{}, 1},
+	}
+
+	for _, test := range tests {
+		result := Grow(test.arr)
+		if result != test.expected {
+			t.Errorf("Grow(%v) = %v; expected %v", test.arr, result, test.expected)
+		}
+	}
+}
