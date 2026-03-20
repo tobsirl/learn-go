ea9114415458ba3e2ca24e497ac8cb8f46978a33
Paul Tobin <tobster111@gmail.com>
2026-03-20 18:32:31 +0000

    Add unit tests for GetCount function to validate vowel counting

--- codewars/vowel_count/vowel_count_test.go
+++ codewars/vowel_count/vowel_count_test.go
@@ -1,2 +1,22 @@
 package vowelcount
 
+import "testing"
+
+func TestGetCount(t *testing.T) {
+	tests := []struct {
+		str      string
+		expected int
+	}{
+		{"hello", 2},
+		{"world", 1},
+		{"golang", 2},
+		{"", 0},
+	}
+
+	for _, test := range tests {
+		result := GetCount(test.str)
+		if result != test.expected {
+			t.Errorf("GetCount(%q) = %v; expected %v", test.str, result, test.expected)
+		}
+	}
+}
